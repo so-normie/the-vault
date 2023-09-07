9ab1062b10afec7bcdf7a3d3ac1299b06b6f36c6
Edmund Leibert III <edmund.leibert@outlook.com>
2023-09-06 19:13:13 -0700

    🚧 dev[wip]: Going through leetcode now

--- scripts/migrate-content-to-quartz.ps1
+++ scripts/migrate-content-to-quartz.ps1
@@ -3,7 +3,7 @@ $sourceDir = "C:\Users\edmun\Projects\Repositories\the-vault"
 $destDir = "C:\Users\edmun\Projects\Repositories\quartz\content"
 
 # Directories and files to exclude
-$excludedDirectories = @("templates", ".obsidian", ".github", ".trash", "anki", "scripts")
+$excludedDirectories = @("templates", ".obsidian", ".github", ".trash", "anki-flashcards", "scripts")
 $excludedFiles = @(".gitignore", "README.md", ".gitmodules")
 
 # Check if destination directory exists, if not, create it
@@ -11,34 +11,11 @@ if (-not (Test-Path $destDir)) {
     New-Item -ItemType Directory -Force -Path $destDir
 }
 
-# Function to sanitize filenames
-function Sanitize-FileName($fileName) {
-    # Remove emojis
-    $fileName = [regex]::replace($fileName, '\p{So}', '')
-
-    # Remove leading and trailing spaces
-    $fileName = $fileName.Trim()
-
-    # Remove single and double quotes
-    $fileName = $fileName -replace "[`"']",''
-
-    # Replace special colons and double colons with hyphens
-    $fileName = $fileName -replace "꞉꞉|::", '-'
-
-    # Replace "꞉ " with "-"
-    $fileName = $fileName -replace "꞉ ", '-'
-
-    # Remove periods right after a number
-    $fileName = $fileName -replace "(?<=\d)\.", ''
-
-    return $fileName
-}
-
 # Copy files and folders
 Get-ChildItem -Path $sourceDir -Recurse | ForEach-Object {
     $exclude = $False
     foreach ($dir in $excludedDirectories) {
-        if ($_.FullName -match "\\$dir\\") {
+        if ($_.FullName.Contains("\$dir\")) {
             $exclude = $True
             break
         }
@@ -47,12 +24,6 @@ Get-ChildItem -Path $sourceDir -Recurse | ForEach-Object {
         Write-Host "Skipping excluded directory or file: $($_.FullName)"
     } else {
         $destination = $destDir + $_.FullName.Substring($sourceDir.Length)
-
-        # Check if sanitization is needed and if so, sanitize the filename
-        $sanitizedDestination = Sanitize-FileName $destination
-        if ($sanitizedDestination -ne $destination) {
-            $destination = $sanitizedDestination
-        }
 
         if ($_.PSIsContainer) {
             New-Item -ItemType Directory -Force -Path $destination
